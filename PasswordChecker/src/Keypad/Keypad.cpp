13455b3d466581691f5badf2ae71abdc30debf09
Bogdan Dumitrescu <bogdan.dumitrescu02@stud.acs.upb.ro>
2024-05-04 20:44:25 +0300

    keypad class working

---
+++ PasswordChecker/src/Keypad/Keypad.cpp
@@ -0,0 +1,70 @@
+#include "Keypad.h"
+
+Keypad::Keypad(const int numRows, const int numCols, const int *rowPins, const int *colPins, const char **keys)
+    : numRows(numRows), numCols(numCols), rowPins(rowPins), colPins(colPins), keys(keys), lastKeyPressed('Z')
+{
+}
+
+int Keypad::getLastKeyPressed()
+{
+    return lastKeyPressed;
+}
+
+void Keypad::setLastKeyPressed(char key)
+{
+    lastKeyPressed = key;
+}
+
+void Keypad::initialize()
+{
+    // Set row pins as inputs and enable pull-up resistorss
+    for (int i = 0; i < numRows; i++)
+    {
+        pinMode(rowPins[i], INPUT_PULLUP);
+    }
+
+    // Set column pins as outputs
+    for (int i = 0; i < numCols; i++)
+    {
+        pinMode(colPins[i], OUTPUT);
+        digitalWrite(colPins[i], HIGH); // Set columns to high to disable
+    }
+}
+
+char Keypad::getKey()
+{
+    for (int col = 0; col < numCols; col++)
+    {
+        // Enable the current column
+        digitalWrite(colPins[col], LOW);
+
+        // Check each row in this column
+        for (int row = 0; row < numRows; row++)
+        {
+            // If a key is pressed (LOW), return its value
+            if (digitalRead(rowPins[row]) == LOW)
+            {
+                // Disable the current column
+                digitalWrite(colPins[col], HIGH);
+                return keys[row][col];
+            }
+        }
+
+        // Disable the current column
+        digitalWrite(colPins[col], HIGH);
+    }
+
+    // No key pressed, clear the flag
+    lastKeyPressed = 'Z';
+    return '\0'; // Return null if no key is pressed
+}
+
+void Keypad::printPressedKey(char key)
+{
+    if (key != '\0')
+    {
+        Serial.println("Key: " + String(key));
+        delay(250);
+    }
+    // Add LCD functionality here if needed
+}
---
+++ PasswordChecker/src/Keypad/Keypad.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef KEYPAD_H
+#define KEYPAD_H
+
+#include <Arduino.h>
+
+class Keypad
+{
+private:
+    int numRows;
+    int numCols;
+    const int *rowPins;
+    const int *colPins;
+    const char **keys;
+    int lastKeyPressed;
+
+public:
+    Keypad(const int numRows, const int numCols, const int *rowPins, const int *colPins, const char **keys);
+    void initialize();
+    char getKey();
+    void printPressedKey(char key);
+    int getLastKeyPressed();
+    void setLastKeyPressed(char key);
+};
+
+#endif
